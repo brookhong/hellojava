81521baa3ae59925c1b972e54ed554ccbe8551ed
brook hong <hzgmaxwell@hotmail.com>
2018-07-22 11:13:26 +0800

    use lombok.extern.slf4j.Slf4j

--- src/main/java/HelloLog4j.java
+++ src/main/java/HelloLog4j.java
@@ -1,9 +1,10 @@
 import org.apache.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class HelloLog4j {
-    private static Logger logger = Logger.getLogger(HelloLog4j.class);
     public static void main(String[] args) {
-        logger.debug("This is debug message.");
-        logger.info("This is info message.");
-        logger.error("This is error message.");
+        log.info("This is info message.");
+        log.error("This is error message.");
     }
 }
