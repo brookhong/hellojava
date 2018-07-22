3c7655349239586dcc1072a2bcb032d59f2c776f
brook hong <hzgmaxwell@hotmail.com>
2018-07-22 19:09:21 +0800

    use lombok.AllArgsConstructor and lombok.ToString

---
+++ src/main/java/H1Template.java
@@ -0,0 +1,20 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class H1Template {
+     private String locale;
+     private String pageId;
+     private Tags template;
+
+     /**
+      * H1 data, currently only include h1 content.
+      */
+     @AllArgsConstructor
+     @ToString
+     public static class Tags {
+         private String content;
+     }
+ }
--- src/main/java/HelloLog4j.java
+++ src/main/java/HelloLog4j.java
@@ -4,7 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HelloLog4j {
     public static void main(String[] args) {
-        log.info("This is info message.");
-        log.error("This is error message.");
+        H1Template h1 = new H1Template("zh_CN", "B004IJO7K8", new H1Template.Tags("/{desc}/{pageId}/"));
+        log.error("This is debug message.");
+        log.error(h1.toString());
     }
 }
